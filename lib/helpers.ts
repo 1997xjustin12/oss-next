23f2ebaa936ae62539818a0fbe1d0b9a848de2e7
1997xjustin12 <1997justin12@gmail.com>
2026-06-23 18:14:50 +0800

    dev base url as fallback

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'https://oss-next.vercel.app/';
 export const CONTACT_NUMBER = "(888) 977-9085";
